acbc593c66cad271f6e2518f6e8768c27c7f128f
Gabriel Teles <gabrieltbeserra@gmail.com>
2020-08-06 23:32:34 -0300

    Fisrt Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'react-calendar-heatmap/dist/styles.css';
+
+import React /*, { useState }*/ from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+//import { ThemeProvider } from 'styled-components';
+import Infos from './Pages/Infos';
+import Header from './components/header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Infos />} />
+      </Routes>
+
+      {/*<Footer />*/}
+
+      {/*<GlobalStyles />*/}
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+const header: React.FC = () => {
+  return (
+    <h1>Teste</h1>
+  );
+}
+
+export default header;
